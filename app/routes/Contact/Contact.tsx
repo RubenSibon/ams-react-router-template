adbd7a00dd0451f80b3d7d735aaa4ef9c963b1e1
Ruben Sibon <r.sibon@amsterdam.nl>
2026-06-19 11:39:56 +0200

    feat(Contact): Add internal & external links

--- app/routes/Contact/Contact.tsx
+++ app/routes/Contact/Contact.tsx
@@ -1,10 +1,32 @@
-import { Grid, Paragraph } from "@amsterdam/design-system-react";
+import { Grid, Heading, LinkList } from "@amsterdam/design-system-react";
+import {
+  HouseIcon,
+  PhoneIcon,
+  SpeechBalloonEllipsisIcon,
+} from "@amsterdam/design-system-react-icons";
 
 export function Contact() {
   return (
     <Grid>
-      <Grid.Cell span="all">
-        <Paragraph>Contact</Paragraph>
+      <Grid.Cell className="ams-prose" span="all">
+        <Heading level={1}>Contact</Heading>
+        <LinkList>
+          <LinkList.Link href="/contact" icon={<SpeechBalloonEllipsisIcon />}>
+            Contactformulier
+          </LinkList.Link>
+          <LinkList.Link
+            href="https://www.amsterdam.nl/contact/adressen-openingstijden/"
+            icon={<HouseIcon />}
+          >
+            Adressen en openingstijden
+          </LinkList.Link>
+          <LinkList.Link
+            href="https://formulieren.amsterdam.nl/TriplEforms/DirectRegelen/formulier/nl-NL/evAmsterdam/contactformulier.aspx/"
+            icon={<PhoneIcon />}
+          >
+            Bel 14 020
+          </LinkList.Link>
+        </LinkList>
       </Grid.Cell>
     </Grid>
   );
